d7db90216a6bead900a73bd3975d94c28738f328
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2020-02-27 22:20:49 +0100

    First version of boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+  },
+  rules:  {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "@typescript-eslint/no-explicit-any": 1,
+    "@typescript-eslint/no-unused-vars": "warn"
+  },
+};
